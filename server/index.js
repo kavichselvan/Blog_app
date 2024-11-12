d1ac152e2eec4ff636dc7571a83ae6e1e7ed67c4
Kavichselvan2001@gmail.com <kavichselvan2001@gmail.com>
2024-11-12 11:42:01 +0530

    updated new

--- server/index.js
+++ server/index.js
@@ -1,7 +1,7 @@
 const express=require('express')
 const app=express()
 const mongoose=require('mongoose')
-const dotenv=require('dotenv').config()
+const dotenv=require('dotenv')
 const cors=require('cors')
 const multer=require('multer')
 const path=require("path")
