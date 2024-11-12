88e132fa0333d012cbe4a64991f9e834d143d85d
Kavichselvan2001@gmail.com <kavichselvan2001@gmail.com>
2024-11-12 12:50:23 +0530

    updated cors

--- server/index.js
+++ server/index.js
@@ -32,7 +32,10 @@ const connectDB=async()=>{
 dotenv.config()
 app.use(express.json())
 app.use("/images",express.static(path.join(__dirname,"/images")))
-app.use(cors({origin:"https://bluetoblog.netlify.app/",credentials:true}))
+app.use(cors({
+    origin: "*",  // Allow all origins (use with caution)
+    credentials: true
+}));
 app.use(cookieParser())
 app.use("/api/auth",authRoute)
 app.use("/api/users",userRoute)
