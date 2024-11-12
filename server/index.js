f32629aba2b0e20441d0309883155795993f37ca
Kavichselvan2001@gmail.com <kavichselvan2001@gmail.com>
2024-11-12 12:44:35 +0530

    updated frontend url

--- server/index.js
+++ server/index.js
@@ -32,7 +32,7 @@ const connectDB=async()=>{
 dotenv.config()
 app.use(express.json())
 app.use("/images",express.static(path.join(__dirname,"/images")))
-app.use(cors({origin:"http://localhost:3000",credentials:true}))
+app.use(cors({origin:"https://bluetoblog.netlify.app/",credentials:true}))
 app.use(cookieParser())
 app.use("/api/auth",authRoute)
 app.use("/api/users",userRoute)
