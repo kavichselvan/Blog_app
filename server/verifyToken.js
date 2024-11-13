7a832eaa21c5c268304bf8c7d37c943b42b17885
Kavichselvan2001@gmail.com <kavichselvan2001@gmail.com>
2024-11-13 10:09:05 +0530

    modified server files

--- server/verifyToken.js
+++ server/verifyToken.js
@@ -1,7 +1,7 @@
 const jwt=require('jsonwebtoken')
 
 const verifyToken=(req,res,next)=>{
-    const token=req.cookies.token
+    const token=req.session.token
     // console.log(token)
     if(!token){
         return res.status(401).json("You are not authenticated!")
